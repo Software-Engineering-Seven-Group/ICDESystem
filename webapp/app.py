9a5c2899b87fe21f55889731ed6d83818d14d9d4
SilverGamer <1795959147@qq.com>
2023-10-01 23:45:36 -0400

    #4 add Pie Graph and interact with back end

--- webapp/app.py
+++ webapp/app.py
@@ -7,10 +7,23 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/Tripedia"
 app.secret_key = "mysecretkey"
 mongo = PyMongo(app)
 
+
 "“首页，重定向”"
 @app.route('/')
 def home():
-    return redirect(url_for('register'))
+    #return redirect(url_for('register'))
+
+    analysed_data = {"America" :  14, "China" :  10, "India": 20, "France": 50, "England" : 6}
+
+    analysed_data_keys = list(analysed_data.keys())
+    analysed_data_values = list(analysed_data.values())
+
+    print(analysed_data_values)
+    print(analysed_data_keys)
+
+    return render_template("home.html", data_keys_list = analysed_data_keys, data_values_list = analysed_data_values, loginstate = '')
+
+
 "插入信息（注册用户信息）"
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -85,5 +98,6 @@ def search():
         exist_datas=list(copy.deepcopy(exist_data))
         return render_template('search.html',results=exist_datas)
     return render_template('search.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
