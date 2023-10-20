9834bbf126957a38113c519d2770a168b4f4cd09
Yuhang Chen <waterkingest@gmail.com>
2023-10-20 00:00:11 -0400

    Show hotels images

--- webapp/search.py
+++ webapp/search.py
@@ -1,14 +1,19 @@
-from flask import render_template, request, redirect, url_for, Blueprint,flash
+from flask import render_template, request, redirect, url_for, Blueprint,flash,jsonify,render_template
 from database_manager import user_preference_infor_manager
 from database_manager import MongoDBManager, UserInfoCollection, mongo_manager, user_infor_manager, user_preference_infor_manager
 import copy
-search_api = Blueprint('search_api', __name__)
 from crawler.Get_Hotel import Get_booking_hotel
+from flask_cors import CORS
+
+search_api = Blueprint('search_api', __name__)
+
+CORS(search_api)
 @search_api.route('/search', methods=['POST', 'GET'])  # Search
 def search():
     hotels = mongo_manager.get_collection("Hotels")
 
     if request.method == 'POST':
+        # print(request.form)
         Keyword = request.form['Keyword']
         checkin = request.form['checkin']
         checkout = request.form['checkout']
@@ -27,7 +32,7 @@ def search():
                 insert_data = {
                     'Hotel_name': each_data['displayName']['text'],
                     'Location': each_data['location']['displayLocation'],
-                    'Images': 'https://cf.bstatic.com/' +
+                    'Images': 'https://cf.bstatic.com' +
                               each_data['basicPropertyData']['photos']['main']['highResJpegUrl']['relativeUrl'],
                     'score': each_data['basicPropertyData']['reviewScore']['score'],
                     'Price': each_data['blocks'][0]['finalPrice']['amount'],
@@ -35,8 +40,10 @@ def search():
                 }
                 # print(insert_data)
                 hotels.insert_one(insert_data)
-        flash('搜索成功')
+        # flash('搜索成功')
         exist_data = hotels.find({"Location": {"$regex": Keyword}}).limit(10)
         exist_datas = list(copy.deepcopy(exist_data))
+        # exist_datas = copy.deepcopy(exist_data)
         return render_template('search.html', results=exist_datas)
+        # jsonify(exist_datas)
     return render_template('search.html')
