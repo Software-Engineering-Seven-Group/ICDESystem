bf5c0e314a1d0b1e1cc0acb02befa721ed1d6f1b
SilverGamer <1795959147@qq.com>
2023-10-29 19:01:41 -0400

    #0 update the functions of analysis words

--- webapp/data_displayer.py
+++ webapp/data_displayer.py
@@ -1,8 +1,40 @@
 from flask import  session
 from flask import render_template, request, redirect, url_for, Blueprint
 from data_analyser import KNNDataAnalayzer
+from database_manager import city_infor_manager, user_preference_infor_manager
 
 displayer_api = Blueprint('displayer_api', __name__)
+
+def generate_introduce_of_the_recommend_city(city_name, user_name):
+    generate_introduce = 'Bonjour! According to the psychological testing, the best tourist destination for your is ' + city_name + '. '
+
+    #About work load
+    user_workload = user_preference_infor_manager.get_daily_workload(user_name)
+    city_nature = city_infor_manager.get_city_natural(city_name)
+    if user_workload > 50:
+        generate_introduce =  generate_introduce + city_name + ' has awsome natural environment (the natural rate is ' + str(city_nature)  + '), you can release your work load and enjoy yourself. '
+
+    user_history = user_preference_infor_manager.get_prefer_history(user_name)
+    city_history = city_infor_manager.get_city_historical(city_name)
+    if user_history > 50:
+        generate_introduce = generate_introduce + city_name + ' is also a famous historical city (the historical rate is ' + str(city_history) + '), you can find a great number of interesting history stories here. '
+
+    user_culture = user_preference_infor_manager.get_prefer_culture(user_name)
+    city_culture = city_infor_manager.get_city_humanistic(city_name)
+    if user_culture > 50:
+        generate_introduce = generate_introduce + city_name + ' is well-known because of her culture (the historical rate is ' + str(city_culture) + '), you can meet interesting people here. '
+
+    user_solitude = user_preference_infor_manager.get_prefer_solitude(user_name)
+    city_solitude  = city_infor_manager.get_city_metropolis(city_name)
+    if user_solitude < 50:
+        generate_introduce = generate_introduce + city_name + ' is a peace place to relax (the city rate is ' + str(city_solitude) + '), you can find a great number of friends. '
+
+    return generate_introduce
+
+def generate_introduce_of_the_popular_city(city_name):
+    generate_introduce = 'In this season, the most popular city is ' + city_name + '. '
+    return generate_introduce
+
 
 # preprocess of data before enter analysis page
 def enter_analysis_page():
@@ -23,13 +55,16 @@ def enter_analysis_page():
 
     popular_cities_name_list, popular_cities_priority_list = analyzer.popular_cities()
 
+    preference_introduction =  generate_introduce_of_the_recommend_city(recommend_cities_name_list[4], session['username'])
+    popular_introduction = generate_introduce_of_the_popular_city(popular_cities_name_list[0])
 
     print(recommend_rates)
     print(recommend_rates)
     print(recommend_rates)
 
     return render_template("analysis.html", data_keys_list=recommend_cities_name_list, data_values_list=recommend_rates,
-                           data_keys_list_popular=popular_cities_name_list, data_values_list_popular=popular_cities_priority_list)
+                           data_keys_list_popular=popular_cities_name_list, data_values_list_popular=popular_cities_priority_list,
+                           recommend_city_word = preference_introduction, popular_city_word = popular_introduction)
 
 #process analysis logic
 @displayer_api.route('/analysis')
